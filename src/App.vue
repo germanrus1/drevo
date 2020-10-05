2cd3019a626c41c3fb057f26077157d290796051
German Salyukin <shuriksful@gmail.com>
2020-10-05 19:08:01 +0400

    Подключил bootstrap

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,24 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Index msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Index from './components/Index.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Index
   }
 }
 </script>
 
 <style>
+  /*@import 'node_modules/bootstrap/scss/bootstrap';*/
+  /*@import 'node_modules/bootstrap-vue/src/index.scss';*/
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
---
+++ src/components/Index.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="hello">
+    <h1>Главная страница</h1>
+    <p>
+      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
+    </p>
+    <b-button variant="success">Button</b-button>
+    <b-button variant="outline-primary">Button</b-button>
+  </div>
+</template>
+
+<script>
+  import { CardPlugin, TablePlugin, BButton } from 'bootstrap-vue'
+  import Vue from 'vue'
+
+  Vue.use(CardPlugin)
+  Vue.use(TablePlugin)
+  Vue.use(BButton)
+
+  export default {
+  name: 'Index',
+  props: {
+    msg: String
+  }
+}
+</script>
+  import 'bootstrap/dist/css/bootstrap.css'
+  import 'bootstrap-vue/dist/bootstrap-vue.css'
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+</style>
